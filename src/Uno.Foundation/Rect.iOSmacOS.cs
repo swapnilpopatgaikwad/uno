2b8e6fc20b36ba25be12ac5802e20d59bb9f4a1b
Martin Zikmund <martin@zikmund.dev>
2022-11-12 11:58:30 +0100

    refactor: Refactoring and documenting Windows.Foundation members

--- src/Uno.Foundation/Rect.iOSmacOS.cs
+++ src/Uno.Foundation/Rect.iOSmacOS.cs
@@ -5,20 +5,19 @@ using System.Linq;
 using System.Text;
 using CoreGraphics;
 
-namespace Windows.Foundation
-{
+namespace Windows.Foundation;
+
     public partial struct Rect
     {
-		/// <summary>
-		/// Converts this <see cref="Rect"/> to a <see cref="CGRect"/>.
-		/// </summary>
-		public CGRect ToCGRect()
-		{
-			return new CGRect(X, Y, Width, Height);
-		}
-
-		public static implicit operator Rect(CGRect rect) => new Rect(rect.X, rect.Y, rect.Width, rect.Height);
-
-		public static implicit operator CGRect(Rect rect) => new CGRect(rect.X, rect.Y, rect.Width, rect.Height);
+	/// <summary>
+	/// Converts this <see cref="Rect"/> to a <see cref="CGRect"/>.
+	/// </summary>
+	public CGRect ToCGRect()
+	{
+		return new CGRect(X, Y, Width, Height);
 	}
+
+	public static implicit operator Rect(CGRect rect) => new Rect(rect.X, rect.Y, rect.Width, rect.Height);
+
+	public static implicit operator CGRect(Rect rect) => new CGRect(rect.X, rect.Y, rect.Width, rect.Height);
 }
